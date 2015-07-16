053d54134d04515ac8c8fe5952529aa960082e70
linhdk <hv@hungvo.org>
2015-07-16 22:28:26 +0700

    First commit

---
+++ foodapp.js
@@ -0,0 +1,16 @@
+(function(){/*****************************************************************************/
+/* Client and Server Routes */
+/*****************************************************************************/
+
+Router.route('Home',{
+  path: '/',
+  template: 'Home',
+});
+
+Router.route('/one');
+
+Router.route('/two');
+
+Router.route('/dashboard');
+
+})();
